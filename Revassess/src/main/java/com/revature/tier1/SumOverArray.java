bde5db139f92e6e064d922a60212f637a71b3769
Kendra Walton <mswalton15@gmail.com>
2020-08-26 14:49:25 -0400

    tier1

--- Revassess/src/main/java/com/revature/tier1/SumOverArray.java
+++ Revassess/src/main/java/com/revature/tier1/SumOverArray.java
@@ -3,6 +3,11 @@ package com.revature.tier1;
 public class SumOverArray {
 
 	public static int IterateAndSum(int[] arr) {
-		return 0;
+		int sum=0;
+		for(int a : arr) {
+			sum+=a;	
+		}System.out.println(sum);
+		return sum;
+		
 	}
 }
